b80cc4e8900ce7be027f1ba35404742107567eb5
kirill22111333 <35836563+kirill22111333@users.noreply.github.com>
2021-06-02 01:31:52 +0300

    Add files via upload

---
+++ 8 kyu/A Needle in the Haystack.php
@@ -0,0 +1,5 @@
+<?php 
+
+function findNeedle($haystack) {
+  return 'found the needle at position '.array_search('needle',$haystack);
+}
---
+++ 8 kyu/Age Range Compatibility Equation.php
@@ -0,0 +1,5 @@
+<?php 
+
+function datingRange($age) {
+  return $age <= 14 ? floor($age - 0.1 * $age) .'-'. floor($age + 0.1 *$age) : floor($age/2+7) .'-'. ($age-7)*2;
+}

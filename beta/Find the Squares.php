392d89dba3e9aecd58ff5acb69c36e21393aefe6
kirill22111333 <35836563+kirill22111333@users.noreply.github.com>
2021-06-02 01:30:24 +0300

    adding

---
+++ beta/Find the Squares.php
@@ -0,0 +1,5 @@
+<?php
+
+    function findsquares($num, $i = 1){
+        while(true) if(++$i**2 - ++$j**2 == $num) return $i**2 . '-' . $j**2;
+    }
---
+++ beta/Number of diagonals.php
@@ -0,0 +1,5 @@
+<?php
+
+    function diagonals($s) {
+        return ($s**2-3*$s)/2;
+    }
